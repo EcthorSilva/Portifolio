1e442ef9b51b195fecb83dd33518c81fcd43dec2
Ecthor Silva <EcthorSilva@outlook.com>
2022-09-15 00:43:45 -0300

    Update EasterEgg Script (#7)

--- js/easter-egg.js
+++ js/easter-egg.js
@@ -1,20 +1,26 @@
 let counterVal = 0;
-let easteregg = document.getElementById("easteregg");
-let intro = document.getElementById("intro");
+let easteregg = document.getElementById("easteregg"); // Div com o EasterEgg
+let intro = document.getElementById("intro"); // Div com as informações da intro
 
-function incrementClick() {
-    console.log('Passou por aqui')
+// Função para contar os clicks e iniciar o easter egg
+function abrir() {
+    console.log('Tentativa ' + counterVal)
     counterVal++;
 
     if (counterVal >= 7) {
-        console.log('Passou por aqui também')
-        if (easteregg.style.display == "flex") {
-            easteregg.style.display = "none";
-            intro.style.display = "flex";
+        console.log('iniciando função...')
+        if (easteregg.style.display != 'flex'){
+            console.log('Terminal Ativado');
+            easteregg.style.display = 'flex';
+            intro.style.display = 'none';
         }
-        else {
-            easteregg.style.display = "flex";
-            intro.style.display = "none";
-        }
+    }
+}
+// Função para finalizar o easter egg
+function fechar() {
+    if(easteregg.style.display == 'flex'){
+        console.log('Terminal Encerrado');
+        easteregg.style.display = 'none';
+        intro.style.display = 'flex';
     }
 }
